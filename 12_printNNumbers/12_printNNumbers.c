3d6ff27c82329980ab311b029bf88d1d729e306d
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-22 19:24:29 +0530

    Print N Numbers
    Using For Loop.

---
+++ 12_printNNumbers/12_printNNumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter The Number: ");
+    scanf("%d",&num);
+
+    for (int i = 1; i < num; i++)
+    {
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
