543d3d951f38a66e719f830db2043b1a99fa045c
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-22 19:30:16 +0530

    Print Even Numbers
    Using For Loop.

---
+++ 13_printEvenNumbers/13_printEvenNumbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter The Number: ");
+    scanf("%d",&num);
+
+    for (int i = 1; i <=num; i++)
+    {
+        if (i%2==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+
+    return 0;
+}
