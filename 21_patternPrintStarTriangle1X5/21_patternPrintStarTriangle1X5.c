cc21622ff950f6c8e70b04416604723514f3163c
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-24 19:22:39 +0530

    Pattern Print Star Triangle 1X5
    *
    * *
    * * *
    * * * *
    * * * * *

---
+++ 21_patternPrintStarTriangle1X5/21_patternPrintStarTriangle1X5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+
+    for (int rows = 1; rows <=5; rows++)
+    {
+        for (int cols=1;cols<=rows; cols++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
