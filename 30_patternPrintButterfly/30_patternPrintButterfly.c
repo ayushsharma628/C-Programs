666e639c30854aa61ebf9600e765f182c8d10f05
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 23:26:39 +0530

    Pattern Print Butterfly
    *        *
    **      **
    ***    ***
    ****  ****
    **********
    ****  ****
    ***    ***
    **      **
    *        *

---
+++ 30_patternPrintButterfly/30_patternPrintButterfly.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    int total_rows;
+
+    printf("Enter The Rows: ");
+    scanf("%d",&total_rows);
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=rows; cols++)
+        {
+            printf("*");
+        }
+        for (int cols = 1; cols<=2*total_rows-2*rows; cols++)
+        {
+            printf(" ");
+        }
+        for (int cols = 1; cols<=rows; cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int rows = 1; rows <total_rows; rows++)
+    {
+        for (int cols=rows;cols<total_rows; cols++)
+        {
+            printf("*");
+        }
+        for (int cols = 1; cols<=2*rows; cols++)
+        {
+            printf(" ");
+        }
+        for (int cols = rows; cols<total_rows; cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
