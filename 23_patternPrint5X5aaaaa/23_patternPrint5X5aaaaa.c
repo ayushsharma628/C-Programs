23729f7153ddf03b91e518d5203dc71ea80cf281
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 11:52:06 +0530

    Pattern Print 5X5 aaaaa
    a a a a a
    b b b b b
    c c c c c
    d d d d d
    e e e e e

---
+++ 23_patternPrint5X5aaaaa/23_patternPrint5X5aaaaa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int alpha ='a';
+
+    for (int rows = 1; rows <=5; rows++)
+    {
+        for (int cols=1;cols<=5; cols++)
+        {
+            printf("%c ",alpha);
+        }
+        printf("\n");
+        alpha++;
+    }
+    
+    return 0;
+}
