caf01f6b72ddc5a2636e882d86da366133118e85
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 22:32:43 +0530

    Pattern Print Odd Star Middle
       *
      ***
     *****
    *******

---
+++ 26_patternPrintStarOddMiddle/26_patternPrintStarOddMiddle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int total_rows;
+
+    printf("ENter The Total ROws: ");
+    scanf("%d",&total_rows);
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=total_rows-rows; cols++)
+        {
+            printf(" ");
+        }
+        for (int cols=1;cols<=2*rows-1; cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
