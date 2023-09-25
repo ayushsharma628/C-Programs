a5202d7c1ba7decfb32bf1ff47e41a31378b8657
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 23:01:56 +0530

    Pattern Print Star Odd Middle Op
    *******
     *****
      ***
       *

---
+++ 29_patternPrintStarOddMiddleOp/29_patternPrintStarOddMiddleOp.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int total_rows;
+
+    printf("ENter The Total ROws: ");
+    scanf("%d",&total_rows);
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=rows-1; cols++)
+        {
+            printf(" ");
+        }
+        for (int cols=rows;cols<=total_rows; cols++)
+        {
+            printf("*");
+        }
+        for (int cols=rows;cols<total_rows; cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
